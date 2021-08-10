5ea7bce245362d0d1c2de26a2cf7411a7cd332a2
Robbie Leib <robert.leib@gmail.com>
2021-08-10 10:40:47 -0700

    prettier

--- example/routes/apps.js
+++ example/routes/apps.js
@@ -1,26 +1,34 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const schema = require('../../lib/schema')
 
-const asycErrors = fn => (
-  (req, res, next) => {
-    const routePromise = fn(req, res, next);
-    if (routePromise.catch) {
-      routePromise.catch(err => {
-        console.log(err)
-        res.render('errors', { err });
-      })
-    }
+const asycErrors = (fn) => (req, res, next) => {
+  const routePromise = fn(req, res, next)
+  if (routePromise.catch) {
+    routePromise.catch((err) => {
+      console.log(err)
+      res.render('errors', { err })
+    })
   }
-);
+}
 
-router.get('/apps', asycErrors(async (req, res) => {
-  res.render('apps', { apps: Object.keys(schema) });
-}))
+router.get(
+  '/apps',
+  asycErrors(async (req, res) => {
+    res.render('apps', { apps: Object.keys(schema) })
+  }),
+)
 
-router.get('/:app', asycErrors(async (req, res) => {
-  const app = req.params.app.replace(/^\w/, (c) => c.toUpperCase())
-  res.render('app', { app, appPath: schema[app].appPath, vertices: schema[app].resources });
-}))
+router.get(
+  '/:app',
+  asycErrors(async (req, res) => {
+    const app = req.params.app.replace(/^\w/, (c) => c.toUpperCase())
+    res.render('app', {
+      app,
+      appPath: schema[app].appPath,
+      vertices: schema[app].resources,
+    })
+  }),
+)
 
 module.exports = router
--- example/server.js
+++ example/server.js
@@ -1,69 +1,74 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const fetch = require('node-fetch');
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const fetch = require('node-fetch')
 const session = require('express-session')
 const PCOApi = require('../lib/pco')
 
-const app = express();
+const app = express()
 app.set('view engine', 'ejs')
-app.set('views', path.resolve(__dirname, 'views'));
-app.use(express.urlencoded({ extended: false }));
-app.set('json spaces', 2);
+app.set('views', path.resolve(__dirname, 'views'))
+app.use(express.urlencoded({ extended: false }))
+app.set('json spaces', 2)
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const redirect = encodeURIComponent('http://localhost:50451/oauth/callback');
+const CLIENT_ID = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET
+const redirect = encodeURIComponent('http://localhost:50451/oauth/callback')
 
-app.use(session({
-  secret: 'isitsecretisitsafe',
-  name: 'uniqueSessionID',
-  saveUninitialized: false,
-  resave: false,
-}))
+app.use(
+  session({
+    secret: 'isitsecretisitsafe',
+    name: 'uniqueSessionID',
+    saveUninitialized: false,
+    resave: false,
+  }),
+)
 
 app.get('/login', (req, res) => {
-  res.redirect(`https://api.planningcenteronline.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${redirect}&response_type=code&scope=people registrations check_ins calendar giving groups services`)
-});
+  res.redirect(
+    `https://api.planningcenteronline.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${redirect}&response_type=code&scope=people registrations check_ins calendar giving groups services`,
+  )
+})
 
 app.get('/logout', async (req, res) => {
-  await fetch(`https://api.planningcenteronline.com/oauth/revoke`,
-    {
-      method: 'POST',
-      body: JSON.stringify( { data: { token: req.session.accessToken }}),
-      headers: {
-        Accept: "application/javascript",
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${req.session.accessToken}`,
-      }
-    });
+  await fetch(`https://api.planningcenteronline.com/oauth/revoke`, {
+    method: 'POST',
+    body: JSON.stringify({ data: { token: req.session.accessToken } }),
+    headers: {
+      Accept: 'application/javascript',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${req.session.accessToken}`,
+    },
+  })
 
   req.session.accessToken = undefined
   req.session.refreshToken = undefined
   req.session.loggedIn = false
 
   res.redirect(`/`)
-});
+})
 
 app.get('/oauth/callback', async (req, res) => {
-  if (!req.query.code) throw new Error('NoCodeProvided');
-  const code = req.query.code;
-  const response = await fetch(`https://api.planningcenteronline.com/oauth/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
+  if (!req.query.code) throw new Error('NoCodeProvided')
+  const code = req.query.code
+  const response = await fetch(
+    `https://api.planningcenteronline.com/oauth/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
     {
-      method: 'POST'
-    });
-  const json = await response.json();
+      method: 'POST',
+    },
+  )
+  const json = await response.json()
   req.session.accessToken = json.access_token
   req.session.refreshToken = json.refresh_token
   req.session.loggedIn = true
   PCOApi.configureClient({ oauthAccessToken: json.access_token })
-  app.use('/', require('./routes/vertex'));
-  res.redirect('/apps');
-});
+  app.use('/', require('./routes/vertex'))
+  res.redirect('/apps')
+})
 
-app.get('/favicon.ico', (req, res) => res.status(204));
+app.get('/favicon.ico', (req, res) => res.status(204))
 
-app.use('/', require('./routes/apps'));
+app.use('/', require('./routes/apps'))
 
 app.get('/', (req, res) => {
   if (req.session.loggedIn) {
@@ -71,8 +76,8 @@ app.get('/', (req, res) => {
   } else {
     res.status(200).render('login')
   }
-});
+})
 
 app.listen(50451, () => {
-  console.info('Running on port 50451');
-});
+  console.info('Running on port 50451')
+})
